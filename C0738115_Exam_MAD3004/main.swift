12934f7db0ede65f732fbfc2e68d61d97b5357e7
MacStudent <macstudent@20X-iMacXX-M-398.local>
2018-07-23 12:57:08 -0400

    second

--- C0738115_Exam_MAD3004/main.swift
+++ C0738115_Exam_MAD3004/main.swift
@@ -8,5 +8,14 @@
 
 import Foundation
 
-print("Hello, World!")
+var v1 = Product(ProductId:100,ProductName: "Harddisk", ProductPrice: 120.0, ProductQuantity: "10")
+var v2 = Product(ProductId:200,ProductName: "flopy", ProductPrice: 40.0, ProductQuantity: "5")
+var v3 = Product(ProductId:300,ProductName: "Zipdrive", ProductPrice: 50.0, ProductQuantity: "7")
+var v4 = Product(ProductId:500,ProductName: "Moniter", ProductPrice: 60.0, ProductQuantity: "8")
+var v5 = Product(ProductId:700,ProductName: "iphone 7 plus", ProductPrice: 40.0, ProductQuantity: "5")
 
+v1.display()
+v2.display()
+v3.display()
+v4.display()
+v5.display()
